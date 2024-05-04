e9c9eee4766b63030925ecf202ae22a48a401cfe
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-05-04 13:45:03 +0530

    commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,72 @@
-// auth ,isStudent,isAdmin
-
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-exports.auth = () => {
+exports.auth = (req, res, next) => {
   try {
-    //extract json webtoken
     const token = req.body.token;
+    // const token = req.cookie.token
+
     if (!token) {
-        return res.status(400).json({
-            message: "token is required",                                                                           
-        })
+      return res.status(401).json({
+        success: false,
+        message: "token missing",
+      });
     }
 
-    // verify token
-
+    // verify the token
     try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-        
+      console.log(decode);
+
+      req.user = decode;
+    } catch (e) {
+      return res.status(401).json({
+        success: false,
+        message: "token is invalid",
+      });
     }
-  } catch (error) {}
+
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({
+      success: false,
+      message: "Something went wrong while verifying token",
+    });
+  }
+};
+
+exports.isStudent = (req, res, next) => {
+  try {
+    if (req.user.role !== "Student") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a protect route for students you can not access it",
+      });
+    }
+    next();
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "User Role is not Matching",
+    });
+  }
+};
+
+exports.isAdmin = (req, res, next) => {
+  try {
+    if (req.user.role !== "Admin") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a protect route for Admins,you can not access it",
+      });
+    }
+    next();
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "User Role is not Matching",
+    });
+  }
 };
